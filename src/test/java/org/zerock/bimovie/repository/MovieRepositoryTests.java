363afde925792672de214b34c48dcc5a033da140
Kimhyunjin4455 <dp4455@naver.com>
2023-07-25 16:59:14 +0900

    0725P640

--- src/test/java/org/zerock/bimovie/repository/MovieRepositoryTests.java
+++ src/test/java/org/zerock/bimovie/repository/MovieRepositoryTests.java
@@ -6,10 +6,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.bimovie.entity.Movie;
 import org.zerock.bimovie.entity.Poster;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @Log4j2
@@ -56,4 +62,57 @@ public class MovieRepositoryTests {
 //        movieRepository.deleteById(1L);
 //        movieRepository.deleteById(2L);
 //    }
+
+    @Test
+    public void insertMovies(){
+        IntStream.rangeClosed(10,100).forEach(i->{ // 10~100까지의 90개
+            Movie movie = Movie.builder().title("세계명작" + i).build();
+
+            movie.addPoster(Poster.builder().fname("세계명작"+i+"포스터1.jpg").build());
+            movie.addPoster(Poster.builder().fname("세계명작"+i+"포스터2.jpg").build());
+
+            movieRepository.save(movie);
+
+        });
+    }
+
+//    @Test
+//    public void testPaging1(){
+//        PageRequest pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+//        Page<Movie> result = movieRepository.findAll(pageable);
+//
+//        result.getContent().forEach(m->{
+//            log.info(m.getMno());
+//            log.info(m.getTitle());
+//            log.info(m.getPosterList().size());
+//            log.info("-------------------------------");
+//        });
+//    }
+
+//    @Test
+//    public void testPaging2All(){
+//        // 문제 발생 >> limit 이 없음
+//        PageRequest pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+//        Page<Movie> result = movieRepository.findAll2(pageable);
+//
+//        result.getContent().forEach(m->{
+//            log.info(m.getMno());
+//            log.info(m.getTitle());
+//            log.info(m.getPosterList());
+//            log.info("-----------------------------");
+//        });
+//    }
+
+    @Test
+    public void testPaging3All(){
+        PageRequest pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+        Page<Object[]> result = movieRepository.findAll3(pageable);
+
+        result.getContent().forEach(arr->{
+            log.info(Arrays.toString(arr));
+            log.info("----------------------------");
+        });
+
+
+    }
 }
